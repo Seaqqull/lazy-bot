95412b6a9ffb8dd86d43324b65cdccf0b039e6d9
Dima <seaqqullprogramming@gmail.com>
2019-05-25 19:53:39 +0300

    Fix Power consumption limitation
     Added:
     - Scene settings
     Changes:
     - Target scene fps 30

---
+++ Assets/Scripts/Scene/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Range(0, 4)] public int vSyncCount = 0;
+    public int MaxFPS = 30;
+
+    void Start()
+    {
+#if UNITY_EDITOR
+        QualitySettings.vSyncCount = vSyncCount;
+        Application.targetFrameRate = MaxFPS;
+#endif
+    }    
+}
