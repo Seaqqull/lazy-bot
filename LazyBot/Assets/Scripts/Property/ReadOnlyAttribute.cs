60b29af6b9a0a029d2b6bdd1a6a27b12b664e86b
Dima <seaqqullprogramming@gmail.com>
2020-02-18 19:38:10 +0100

    Update: ReadOnly editor property
     Description:
     - Attribute tag, that can restrict to read-only access to variable in the editor

---
+++ LazyBot/Assets/Scripts/Property/ReadOnlyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Displays a read-only field in the inspector.
+/// Doesn't work with CustomPropertyDrawers.
+/// For group read-only restriction use <see cref="BeginReadOnlyGroupAttribute"/>
+/// and <see cref="EndReadOnlyGroupAttribute"/>.
+/// </summary>
+public class ReadOnlyAttribute : PropertyAttribute { }
+
+/// <summary>
+/// Begins a read-only group of one or more fields in the inspector.
+/// Used with <see cref="EndReadOnlyGroupAttribute"/> to close the group.
+/// Works with CustomPropertyDrawers.
+/// </summary>
+public class BeginReadOnlyGroupAttribute : PropertyAttribute { }
+
+/// <summary>
+/// Closes the read-only group and resume editable fields.
+/// Used with <see cref="BeginReadOnlyGroupAttribute"/>.
+/// </summary>
+public class EndReadOnlyGroupAttribute : PropertyAttribute { }
